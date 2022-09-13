fc3b01b2f7bf60aa3a140a92b13137eb74abdecd
= <prathameshks2003@gmail.com>
2022-09-13 19:03:46 +0530

    added document for DSL assignment

--- FDSL 1/assignment2.py
+++ FDSL 1/assignment2.py
@@ -7,6 +7,7 @@ c) Count of students who were absent for the test
 d) Display mark with highest frequency
 '''
 
+# global lists for present absent total students
 marks_of_stu = []
 present_stu_marks = []
 absent_stu_list = []
@@ -35,10 +36,16 @@ def find_average():
 	print("Average of the class is %.2f"%(sum/n))
 
 def display_min_max():
+	# initialize lowest and highest marks to index 0
 	lowest = highest = present_stu_marks[0]
-	
 	for mark in present_stu_marks:
-		pass
+		if mark<lowest:
+			lowest = mark
+		if mark>highest:
+			highest=mark
+	
+	print("Highest score of the class is", highest)
+	print("Lowest score of the class is", lowest)
 
 
 def find_max_frequency():
@@ -51,14 +58,51 @@ def find_max_frequency():
 		else:
 			index = marks.index(i)
 			freq[index] += 1
+	max_mark = []
+	max = freq[0]
+	for i in range(len(freq)):
+		if freq[i]>max:
+			max = freq[i]
 	
+	for i in range(len(freq)):
+		if freq[i] == max:
+			max_mark.append(marks[i])
+	if len(max_mark) == 1:
+		print("The marks with highest frequency is", max_mark[0])
+	else:
+		print("The marks with highest frequency are", max_mark)
+
 add_marks()
 
-print(marks_of_stu ,present_stu_marks ,absent_stu_list ,sep='\n')
+while True:
+	print("--MENU--")
+	print("1. The Average score ")
+	print("2. Highest and Lowest score")
+	print("3. Count of students who were absent for the test")
+	print("4. Display mark with highest frequency")
+	print("5. Re Enter the marks")
+	print("6. Exit the code")
+	choice = input("Enter your choice :")
 
-display_min_max()
+	if choice == '1':
+		find_average()
+	elif choice == '2':
+		display_min_max()
+	elif choice == '3':
+		print("The number of students absent for the test are", len(absent_stu_list))
+	elif choice == '4':
+		find_max_frequency()
+	elif choice == '5':
+		add_marks()
+	elif choice == '6':
+		break
+	else:
+		print("Please Enter a valid choice.")
+	print("-"*50)
+print("Successfully Exited")
+input()
 """
-7
+10
 4
 A
 10
@@ -66,5 +110,8 @@ a
 -1
 9
 5
+4
+5
+4
 
 """
