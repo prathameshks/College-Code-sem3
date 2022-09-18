c5b595faa90f4eb6cb33e8ebab56f06c405459b9
= <prathameshks2003@gmail.com>
2022-09-18 21:58:13 +0530

    dsl assign 3 code done

--- FDSL 1/assignment3.py
+++ FDSL 1/assignment3.py
@@ -22,46 +22,37 @@ def read(row,col):
 	return matrix
 
 def addition(mat1,mat2):
-	r1,r2 = len(mat1),len(mat2)
-	c1,c2 = len(mat1[0]),len(mat2[0])
+	r1 = len(mat1)
+	c1 = len(mat1[0])
 	add = []
-	if ((r1!=r2) or (c1!=c2)):
-		return 0
-	else:
-		for i in range(r1):
-			add.append([])
-			for j in range(c1):
-				add[i].append(mat1[i][j]+mat2[i][j])
-		return add
+	for i in range(r1):
+		add.append([])
+		for j in range(c1):
+			add[i].append(mat1[i][j]+mat2[i][j])
+	return add
 		
 def subtraction(mat1,mat2):
-	r1,r2 = len(mat1),len(mat2)
-	c1,c2 = len(mat1[0]),len(mat2[0])
+	r1 = len(mat1)
+	c1 = len(mat1[0])
 	sub = []
-	if ((r1!=r2) or (c1!=c2)):
-		return 0
-	else:
-		for i in range(r1):
-			sub.append([])
-			for j in range(c1):
-				sub[i].append(mat1[i][j]-mat2[i][j])
-		return sub
+	for i in range(r1):
+		sub.append([])
+		for j in range(c1):
+			sub[i].append(mat1[i][j]-mat2[i][j])
+	return sub
 
 def multiplication(mat1,mat2):
-	r1,r2 = len(mat1),len(mat2)
+	r1 = len(mat1)
 	c1,c2 = len(mat1[0]),len(mat2[0])
 	multi = []
-	if (c1!=r2):
-		return 0
-	else:
-		for i in range(r1):
-			multi.append([])
-			for j in range(c2):
-				sum = 0
-				for k in range(c1):
-					sum += mat1[i][k]*mat2[k][j]
-				multi[i].append(sum)
-		return multi
+	for i in range(r1):
+		multi.append([])
+		for j in range(c2):
+			sum = 0
+			for k in range(c1):
+				sum += mat1[i][k]*mat2[k][j]
+			multi[i].append(sum)
+	return multi
 
 def transpose(matrix):
 	trans = []
@@ -78,9 +69,10 @@ def main():
 	while True:
 		print("MENU".center(50,'-'))
 		print("1. Addition of two matrices ")
-		print("2. Subtraction of two matrices")
+		print("2. Subtraction of two matrices ")
 		print("3. Multiplication of two matrices ")
-		print("4. Transpose of a matrix")
+		print("4. Transpose of a matrix ")
+		print("5. Exit the program ")
 		ch = input("Enter your choice :")
 		if ch == '1':
 			can_add = False
@@ -95,41 +87,80 @@ def main():
 					print("Order of both Matrix should be Same to Perform Addition.")
 				else:
 					can_add = True
-			print("*Enter Matrix 1")
+			print("--Enter Matrix 1")
 			mat1 = read(r1,c1)
-			print("*Enter Matrix 2")
+			print("--Enter Matrix 2")
 			mat2 = read(r2,c2)
-			
+			res = addition(mat1,mat2)
+			print("Addition of Matrix ")
+			display(mat1)
+			print("&")
+			display(mat2)
+			print(" is ")
+			display(res)
+		elif ch == '2':
+			can_sub = False
+			while not can_sub:
+				r1 = int(input("Enter number of rows in matrix 1:"))
+				c1 = int(input("Enter number of columns in matrix 1:"))
+				
+				r2 = int(input("Enter number of rows in matrix 2:"))
+				c2 = int(input("Enter number of columns in matrix 2:"))
 
+				if ((r1!=r2) or (c1!=c2)):
+					print("Order of both Matrix should be Same to Perform Subtraction.")
+				else:
+					can_sub = True
+			print("--Enter Matrix 1")
+			mat1 = read(r1,c1)
+			print("--Enter Matrix 2")
+			mat2 = read(r2,c2)
+			res = subtraction(mat1,mat2)
+			print("Subtraction of Matrix ")
+			display(mat1)
+			print("&")
+			display(mat2)
+			print(" is ")
+			display(res)
+		elif ch == '3':
+			can_multi = False
+			while not can_multi:
+				r1 = int(input("Enter number of rows in matrix 1:"))
+				c1 = int(input("Enter number of columns in matrix 1:"))
+				
+				r2 = int(input("Enter number of rows in matrix 2:"))
+				c2 = int(input("Enter number of columns in matrix 2:"))
 
-			
-def main1():
-	print("MENU".center(50,'-'))
-	m = read(4,3)
-	# n = read(3,3)
-	print('matrix 1')
-	display(m)
-	# print('matrix 2')
-	# display(n)
-	ans = transpose(m)
-	if ans:
-		print('Ans is')
-		display(ans)
-	else:
-		#print("Order of both Matrix should be Same to Perform Subtraction.")
-		# print("Number of columns of Matrix 1 should be equal to Number of rows of Matrix 2 for Performing Multiplication.")
-		print("trans error")
+				if (c1!=r2):
+					print("Number of columns of Matrix 1 should be equal to Number of rows of Matrix 2 for Performing Multiplication.")
+				else:
+					can_multi = True
+			print("--Enter Matrix 1")
+			mat1 = read(r1,c1)
+			print("--Enter Matrix 2")
+			mat2 = read(r2,c2)
+			res = multiplication(mat1,mat2)
+			print("Multiplication of Matrix ")
+			display(mat1)
+			print("&")
+			display(mat2)
+			print(" is ")
+			display(res)
+		elif ch == '4':
+			r1 = int(input("Enter number of rows in matrix :"))
+			c1 = int(input("Enter number of columns in matrix :"))
+				
+			print("--Enter Matrix ")
+			mat1 = read(r1,c1)
+			res = transpose(mat1)
+			print("Transpose of Matrix is ")
+			display(mat1)
+			print(" is ")
+			display(res)
+		elif ch == '5':
+			print("Thank you for using Application :)")
+			break
+		else:
+			print("Please enter a valid choice.")
 
 main()
-
-"""
-1
-2
-3
-6
-9
-5
-8
-4
-
-""",
