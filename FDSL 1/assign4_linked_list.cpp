d5bee210280e8d0e8fa1368acbd334add5bff68a
Prathamesh Sable <82529994+prathameshks@users.noreply.github.com>
2022-09-21 09:42:31 +0530

    Add files via upload
    assign 4 code backup

---
+++ FDSL 1/assign4_linked_list.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+template<class T>
+class Node{
+	public:
+		T data;
+		Node* next;
+		Node(T val){
+			data = val;
+			next = NULL;
+		}
+};
+
+template<class T>
+void print_list(Node<T>* n){
+	while (n != NULL){
+		cout<<n->data<<" ";
+		n=n->next;
+	}
+	cout<<endl;
+}
+
+int main(){
+	Node<int> *node1 = new Node<int>();
+	Node<int> *node2 = new Node<int>();
+	Node<int> *node3 = new Node<int>();
+	Node<int> *node4 = new Node<int>();
+	
+	Node<int> *head=node1;
+	
+	node1->data = 100;
+	node1->next = node2;
+	node2->data = 101;
+	node2->next = node3;
+	node3->data = 102;
+	node3->next = node4;
+	node4->data = 103;
+	node4->next = NULL;
+	print_list(head);
+	return 0;
+}
