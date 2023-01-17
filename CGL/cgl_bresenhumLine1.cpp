f710fc12b23125840700a306dc34ba6d9c4b0418
prathamesh <prathameshks2003@gmail.com>
2023-01-17 21:37:29 +0530

    cg codes + namespace in opp

--- CGL/cgl_bresenhumLine1.cpp
+++ CGL/cgl_bresenhumLine1.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 // algo simple for slope 0 to 1
-void bresenhum_line1(int x1, int y1, int x2, int y2)
+void bresenhum_line(int x1, int y1, int x2, int y2)
 {
     int x, y, dx, dy, p;
     x = x1;
@@ -28,71 +28,12 @@ void bresenhum_line1(int x1, int y1, int x2, int y2)
     }
 }
 
-// all class algo
-int sign(int a)
-{
-    if (a < 0)
-        return -1;
-    else
-        return 1;
-}
-
-void bresenhum_line(int x1, int y1, int x2, int y2)
-{
-    int x, y, dx, dy, sx, sy, e;
-    bool interchange;
-    x = x1;
-    y = y1;
-    dx = abs(x2 - x1);
-    dy = abs(y2 - y1);
-    sx = sign(x2 - x1);
-    sy = sign(y2 - y1);
-    if (dy > dx)
-    {
-        int temp = dy;
-        dy = dx;
-        dx = temp;
-        delete &temp;
-        interchange = true;
-    }
-    else
-    {
-        interchange = false;
-    }
-    e = 2 * dy - dx;
-
-    for (int i = 0; i < dx; i++)
-    {
-        putpixel(x, y, RED);
-        while (e > 0)
-        {
-            if (interchange)
-            {
-                x += sx;
-            }
-            else
-            {
-                y += sy;
-            }
-            e -= 2 * dx;
-        }
-        if (interchange)
-        {
-            y += sy;
-        }
-        else
-        {
-            x += sx;
-        }
-        e += 2 * dy;
-    }
-}
 
 int main()
 {
     int gdriver = DETECT, gmode;
     initgraph(&gdriver, &gmode, NULL);
-    bresenhum_line(250, 250, 50, 100);
+    bresenhum_line(50, 50, 250, 100);
     getch();
     closegraph();
 
