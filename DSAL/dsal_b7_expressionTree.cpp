7de5c4c1d894b43f70efbf1f1af8b763b4823dd9
Prathamesh Sable <82529994+prathameshks@users.noreply.github.com>
2023-03-06 11:52:24 +0530

    pract b7

---
+++ DSAL/dsal_b7_expressionTree.cpp
@@ -0,0 +1,128 @@
+/*
+Construct an expression tree from the given prefix expression eg. +--a*bc/def and
+traverse it using post order traversal (non recursive) and then delete the entire tree.
+*/
+
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+    char data;
+    node* right;
+    node* left;
+    node(){
+        right = NULL;
+        left = NULL;
+    }
+    node(char data){
+        right = NULL;
+        left = NULL;
+        this->data = data;
+    }
+};
+
+class stackNode{
+    public:
+    node* data;
+    stackNode* next;
+};
+
+class stack{
+    stackNode* top;
+    public:
+    stack(){
+        top = NULL;
+    }
+    void push(node* val){
+        if(top == NULL){
+            top = new stackNode();
+            top->data = val;
+        }
+        else{
+            stackNode* newnode = new stackNode();
+            newnode->data = val;
+            newnode->next = top;
+            top = newnode;
+        }
+    }
+    node* pop(){
+        if(top == NULL){
+            return NULL;
+        }else{
+            node* temp = top->data;
+            top = top->next;
+            return temp;
+        }
+    }
+        
+};
+
+
+class ExpressionTree{
+    node* root;
+    
+    stack nodestack();
+    
+    bool isOperator(char op){
+        switch (op){
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                return true;
+            default:
+                return false;
+        }
+    }
+    
+    public:
+    ExpressionTree(){
+        root = NULL;
+    }
+    
+    void insert(char ch){
+        if(isOperator(ch)){
+            node* temp = new node(ch);
+            temp->left = nodestack.pop();
+            temp->right = nodestack.pop();
+            nodestack.push(temp);
+        }else{
+            node* temp = new node(ch);
+            nodestack.push(temp);
+        }
+    }
+    
+    void input_prefix(string exp){        
+        for(int i=exp.length()-1;i>=-1;i--){
+            insert(exp[i]);
+        }
+        root = nodestack.pop();
+    }
+    void display_postfix(){
+        cout<<"Postfix Expression is ";
+        postfix(root);
+        cout<<endl;
+    }
+    void postfix(node* cur){
+        if(cur != NULL){
+            postfix(cur->left);
+            postfix(cur->right);
+            cout<<cur->data;
+        }
+    }
+};
+
+int main(){
+    string st;
+    ExpressionTree tree;
+    cout<<"Enter prefix expression:";
+    cin>>st;
+    
+    tree.input_prefix(st);
+    
+    tree.display_postfix();
+    
+    
+    return 0;
+}
