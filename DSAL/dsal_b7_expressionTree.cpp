7d81ef30bb0ed0de9eabb5f39e8cf3bed51689e4
Prathamesh <prathameshks2003@gmial.com>
2023-03-13 10:33:25 +0530

    expression tree error

--- DSAL/dsal_b7_expressionTree.cpp
+++ DSAL/dsal_b7_expressionTree.cpp
@@ -1,71 +1,70 @@
 /*
-Construct an expression tree from the given prefix expression eg. +--a*bc/def and
-traverse it using post order traversal (non recursive) and then delete the entire tree.
+Construct an expression tree from the given prefix expression eg. +--a*bc/def
+and traverse it using post order traversal (non recursive) and then delete the
+entire tree.
 */
 
-#include<iostream>
+#include <iostream>
+#include <map>
+#include<unordered_map>
 using namespace std;
 
-class node{
-    public:
+class node {
+   public:
     char data;
     node* right;
     node* left;
-    node(){
+    node() {
         right = NULL;
         left = NULL;
     }
-    node(char data){
+    node(char data) {
         right = NULL;
         left = NULL;
         this->data = data;
     }
 };
 
-class stackNode{
-    public:
+class stackNode {
+   public:
     node* data;
     stackNode* next;
 };
 
-class stack{
+class stack {
     stackNode* top;
-    public:
-    stack(){
-        top = NULL;
-    }
-    void push(node* val){
-        if(top == NULL){
+
+   public:
+    stack() { top = NULL; }
+    void push(node* val) {
+        if (top == NULL) {
             top = new stackNode();
             top->data = val;
-        }
-        else{
+        } else {
             stackNode* newnode = new stackNode();
             newnode->data = val;
             newnode->next = top;
             top = newnode;
         }
     }
-    node* pop(){
-        if(top == NULL){
+    node* pop() {
+        if (top == NULL) {
             return NULL;
-        }else{
+        } else {
             node* temp = top->data;
             top = top->next;
             return temp;
         }
     }
-        
 };
 
-
-class ExpressionTree{
+class ExpressionTree {
     node* root;
-    
-    stack nodestack();
-    
-    bool isOperator(char op){
-        switch (op){
+
+    stack nodestack;
+
+    bool isOperator(char op) {
+        switch (op) {
             case '+':
             case '-':
             case '*':
@@ -75,54 +74,75 @@ class ExpressionTree{
                 return false;
         }
     }
-    
-    public:
-    ExpressionTree(){
-        root = NULL;
-    }
-    
-    void insert(char ch){
-        if(isOperator(ch)){
+
+   public:
+    ExpressionTree() { root = NULL; }
+
+    void insert(char ch) {
+        if (isOperator(ch)) {
             node* temp = new node(ch);
             temp->left = nodestack.pop();
             temp->right = nodestack.pop();
             nodestack.push(temp);
-        }else{
+        } else {
             node* temp = new node(ch);
             nodestack.push(temp);
         }
     }
-    
-    void input_prefix(string exp){        
-        for(int i=exp.length()-1;i>=-1;i--){
+
+    void input_prefix(string exp) {
+        for (int i = exp.length() - 1; i >= -1; i--) {
             insert(exp[i]);
         }
         root = nodestack.pop();
     }
-    void display_postfix(){
-        cout<<"Postfix Expression is ";
-        postfix(root);
+
+    void postfix_exp() {
+        node* temp = root;
+        unordered_map<node*, node*> parent;
+        parent.insert(pair<node*,node*> (root,nullptr));
+
+        while (temp) {
+            cout<<temp->data<<"*";
+            if (temp->left && (parent.find(temp->left) == parent.end())){
+                parent[temp->left] = temp;
+                temp = temp->left;
+            }else if (temp->right && (parent.find(temp->right) == parent.end())){
+                parent[temp->right] = temp;
+                temp = temp->right;
+            }else {
+                cout << temp->data;
+                temp = (parent.find(temp))->second;
+            }
+        }
         cout<<endl;
     }
-    void postfix(node* cur){
-        if(cur != NULL){
+
+    void display_postfix() {
+        cout << "Postfix Expression is ";
+        postfix(root);
+        cout << endl;
+    }
+    void postfix(node* cur) {
+        if (cur != NULL) {
             postfix(cur->left);
             postfix(cur->right);
-            cout<<cur->data;
+            cout << cur->data;
         }
     }
 };
 
-int main(){
+int main() {
     string st;
     ExpressionTree tree;
-    cout<<"Enter prefix expression:";
-    cin>>st;
-    
+    // cout << "Enter prefix expression:";
+    // cin >> st;
+    st = "+--a*bc/def";
+
     tree.input_prefix(st);
-    
     tree.display_postfix();
-    
-    
+    tree.postfix_exp();
+
+
     return 0;
 }
